a7945e0c3cfa0a9726ee493634cc1339e1a154ff
Théo Leleu <43882382+theoleleu@users.noreply.github.com>
2021-12-03 21:39:31 +0100

    Update CPPTokenizer.java
    pmd-cpp/src/main/java/net/sourceforge/pmd/cpd/CPPTokenizer.java 
    Add CPP Process Token feature

--- pmd-cpp/src/main/java/net/sourceforge/pmd/cpd/CPPTokenizer.java
+++ pmd-cpp/src/main/java/net/sourceforge/pmd/cpd/CPPTokenizer.java
@@ -87,7 +87,25 @@ public class CPPTokenizer extends JavaCCTokenizer {
             throw new RuntimeException(e);
         }
     }
+    
+    @Override
+    protected TokenEntry processToken(Tokens tokenEntries, GenericToken currentToken, String fileName) {
+        String image = currentToken.getImage();
+        /*Token cppToken = (Token) currentToken;
+        constructorDetector.restoreConstructorToken(tokenEntries, cppToken);
+        if (ignoreLiterals && (javaToken.kind == JavaParserConstants.STRING_LITERAL
+                || javaToken.kind == JavaParserConstants.CHARACTER_LITERAL
+                || javaToken.kind == JavaParserConstants.DECIMAL_LITERAL
+                || javaToken.kind == JavaParserConstants.FLOATING_POINT_LITERAL)) {
+            image = String.valueOf(javaToken.kind);
+        }
+        if (ignoreIdentifiers && javaToken.kind == JavaParserConstants.IDENTIFIER) {
+            image = String.valueOf(javaToken.kind);
+        }
+        constructorDetector.processToken(cppToken);*/
 
+        return new TokenEntry(image, fileName, currentToken.getBeginLine(), currentToken.getBeginColumn(), currentToken.getEndColumn());
+    }
     @Override
     protected TokenFilter getTokenFilter(final TokenManager tokenManager) {
         return new CppTokenFilter(tokenManager, ignoreLiteralSequences);
